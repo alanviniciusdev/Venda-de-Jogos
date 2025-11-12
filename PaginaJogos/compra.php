74027bc9afe7b37414c2878e9a5fda1db80e5c90
alanviniciusdev <viniciusalan2007@gmail.com>
2025-11-12 10:22:58 -0300

    Pagina para o usuario comprar os jogos

---
+++ PaginaJogos/compra.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Compra</title>
+    <link rel="stylesheet" href="css/compra.css">
+</head>
+
+<body>
+    <div class="card-prod">
+        <div class="left-card">
+            <img id="capa" src="" alt="">
+        </div>
+        <div class="right-card">
+            <h1 id="titulo"></h1>
+            <span id="preco"></span>
+            <p id="genero"></p>
+            <p id="plataforma"></p>
+            <p id="desenvolvedora"></p>
+            <p id="estoque"></p>
+
+
+            <div class="btn">
+                <button type="submit" id="btn-comprar">Comprar Agora</button>
+                <button type="submit" id="btn-adicionar">Adicionar no Carrinho</button>
+            </div>
+        </div>
+    </div>
+
+
+
+
+
+    <script src="irparacompra.js"></script>
+</body>
+
+</html>
